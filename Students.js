b639f5ae9654e21af709318112423e8ceb28b4e9
Dineshvj94 <dineshsurya1129@gmail.com>
2026-03-31 13:52:56 +0530

    Restore Students.js file

---
+++ Students.js
@@ -0,0 +1,14 @@
+const students = [
+  {
+    name: "John",
+    subject: "Mathematics",
+    marks: 85
+  },
+  {
+    name: "Sarah",
+    subject: "Mathematics",
+    marks: 92
+  }
+];
+
+console.log(students);
